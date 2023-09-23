ee9a6d4a09d2d86dd6387958b5a3dc2cc318f6f2
Alberto Recio <albertoreciodev@gmail.com>
2023-09-23 18:36:05 +0200

    add test in package & create a dummy test in Jest

---
+++ 01-clase/src/dummy.spec.ts
@@ -0,0 +1,13 @@
+describe('dummy specs', () => {
+  // test('should pass spec', () => {
+
+  // });
+  it('should pass spec', () => {
+    // Arrange - Definimos los parámetros de entrada que necesitamos antes de ejecutar el test o los mock que necesitamos hacer antes de nada
+
+    // Act - Ejecutar la funcionalidad que queremos probar
+
+    // Assert - Verificación
+    expect(true)
+  });
+});
